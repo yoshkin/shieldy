94fd8c280fe0c585bbb6109472950e0f76f6df13
Alexey Yashenkov <alexey.yashenkov@gmail.com>
2022-03-14 10:33:25 +0300

    fix

--- src/middlewares/checkRestrict.ts
+++ src/middlewares/checkRestrict.ts
@@ -1,37 +1,37 @@
-import { Context } from 'telegraf'
-import { isGloballyRestricted } from '@helpers/globallyRestricted'
-import { deleteMessageSafe } from '@helpers/deleteMessageSafe'
+import { Context } from "telegraf";
+import { isGloballyRestricted } from "@helpers/globallyRestricted";
+import { deleteMessageSafe } from "@helpers/deleteMessageSafe";
 
 export async function checkRestrict(ctx: Context, next: () => any) {
-  if (ctx.update.message?.date && ctx.update.message?.text === '/help') {
+  if (ctx.update.message?.date && ctx.update.message?.text === "/help") {
     console.log(
-      'Got to checkRestrict on help',
+      "Got to checkRestrict on help",
       Date.now() / 1000 - ctx.update.message?.date
-    )
+    );
   }
   // Get the message
-  const message = ctx.editedMessage || ctx.message
+  const message = ctx.editedMessage || ctx.message;
   // Continue if there is no message
   if (!message) {
-    return next()
+    return next();
   }
   // Continue if the restrict is off
   if (!ctx.dbchat.restrict) {
-    return next()
+    return next();
   }
   // Don't restrict super admin
   if (ctx.from.id === parseInt(process.env.ADMIN)) {
-    return next()
+    return next();
   }
   // Just delete the message if globally restricted
   if (isGloballyRestricted(ctx.from.id)) {
-    deleteMessageSafe(ctx)
-    return
+    deleteMessageSafe(ctx);
+    return;
   }
   // Check if this user is restricted
   const restricted = ctx.dbchat.restrictedUsers
     .map((u) => u.id)
-    .includes(ctx.from.id)
+    .includes(ctx.from.id);
   // If a restricted user tries to send restricted type, just delete it
   if (
     restricted &&
@@ -45,13 +45,12 @@ export async function checkRestrict(ctx: Context, next: () => any) {
       message.photo ||
       message.video_note ||
       message.video ||
-      message.game
-      // temp: when restricted user tries to send text message, just delete it
+      message.game ||
       message.text)
   ) {
-    deleteMessageSafe(ctx)
-    return
+    deleteMessageSafe(ctx);
+    return;
   }
   // Or just continue
-  return next()
+  return next();
 }
